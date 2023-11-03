f123a11e5c5fbb4e61dcb138d53b4fdb906510d5
zakaryaalgeria <zz194451@gmail.com>
2023-11-03 10:14:17 +0100

    Update the Resume and add data file to make it more dynamique

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,6 +6,7 @@ import 'swiper/css/pagination';
 import { PageTitlte } from "../component/PageTitlte"
 import { Title } from "../component/Title"
 import {motion} from 'framer-motion'
+import { edu_exp, lang_tools } from "../utils/data";
 
 export const Resume = () => {
 
@@ -34,217 +35,45 @@ export const Resume = () => {
         <div className="container flex flex-wrap py-14 md:justify-between justify-center">
           <div className="w-[450px]">
             <Title ele={'Education && Exprience'} />
-            <div className="relative bg-[#fdfdfd] dark:bg-[#252525]  my-16 h-fit border-l-2 border-b-2 border-solid border-[#f5f6f9] dark:border-[#333333] p-4">
-              <div className="absolute z-10 -top-6 left-0 text-[14px] px-2 py-1 border-2 border-solid rounded-2xl border-[#007ced]">
-                2021 / 2022
-              </div>
-              <div className="absolute text-[16px] -top-[18px] font-semibold left-[105px] text-[#666666] dark:text-[#AAAAAA]">
-                Baccalaureate 
-              </div>
-              <div className="indent-8 pt-4 text-[#666666] dark:text-[#d5d5d5]">
-                I obtained my Baccalaureate in the field of Natural and life sciences
-                with an average grade of 16.87.
-              </div>
-            </div>
-            <div className="relative bg-[#fdfdfd] dark:bg-[#252525]  my-16 h-fit border-l-2 border-b-2 border-solid border-[#f5f6f9] dark:border-[#333333] p-4">
-              <div className="absolute z-10 -top-6 left-0 text-[14px] px-2 py-1 border-2 border-solid rounded-2xl border-[#007ced]">
-                2022 / 2023
-              </div>
-              <div className="absolute text-[16px] -top-[28px] font-semibold left-[105px] text-[#666666] dark:text-[#AAAAAA]">
-                 National higher school of computer science -ESTIN- 
-              </div>
-              <div className="indent-8 pt-4 text-[#666666] dark:text-[#d5d5d5]">
-              I commenced my studies at the National Higher School of Computer Science Engineering (ESTIN),
-               delving into the foundational aspects of algorithms, data structures, electronics, and mathematics. 
-               Throughout my academic journey, I proactively ventured into the realm of web development,
-                mastering both front-end and back-end fundamentals through curated online courses and workshops. 
-              </div>
-            </div>
-            <div className="relative bg-[#fdfdfd] dark:bg-[#252525]  my-16 h-fit border-l-2 border-b-2 border-solid border-[#f5f6f9] dark:border-[#333333] p-4">
-              <div className="absolute z-10 -top-6 left-0 text-[14px] px-2 py-1 border-2 border-solid rounded-2xl border-[#007ced]">
-                2023 / 2024
-              </div>
-              <div className="absolute text-[16px] -top-[28px] font-semibold left-[105px] text-[#666666] dark:text-[#AAAAAA]">
-                 National higher school of computer science -ESTIN- 
-              </div>
-              <div className="indent-8 pt-4 text-[#666666] dark:text-[#d5d5d5]">
-                 Pending ....
-              </div>
-            </div>
+            {edu_exp.map((ele,i)=>(
+              <motion.div 
+              key={i} initial={{opacity:0}} animate={{opacity:1}} transition={{duration:.5}}
+              className="relative bg-[#fdfdfd] dark:bg-[#252525]  my-16 h-fit rounded-tr-2xl border-l-2 border-b-2 border-solid border-[#f5f6f9] dark:border-[#333333] p-4"
+              >
+                <div className="flex items-center gap-5">
+                  <div className="w-[150px] text-[14px] px-2 py-1 border-2 text-center border-solid rounded-2xl border-[#007ced]">
+                    {ele.year}
+                  </div>
+                  <div className="text-[16px] font-semibold  text-[#666666] dark:text-[#AAAAAA]">
+                    {ele.school} 
+                  </div>
+                </div>
+                
+                <div className="indent-8 pt-4 text-[#666666] dark:text-[#d5d5d5]">
+                  {ele.desc}
+                </div>
+              </motion.div>
+            ))}
+            
           </div>
           <div className="w-[450px]">
             <Title ele={'Skills && technologie'} />
             <div className="flex gap-8 py-6 justify-center flex-wrap bg-[#fdfdfd] p-6 rounded-3xl dark:bg-[#252525] "  >
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="/tools/html.png" className="w-14 h-14" alt="HTML"/>
-                <div className="pt-2  font-bold">HTML</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="/tools/css.png" className="w-14 h-14" alt="CSS"/>
-                <div className="pt-2 text-center font-bold">CSS</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/motion.png" className="w-14 h-14" alt="F.MOTION"/>
-                <div className="pt-2 text-center font-bold">F.MOTION</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/js.png" className="w-14 h-14" alt="JAVASCRIPT"/>
-                <div className="pt-2 text-center font-bold">JAVASCRIPT</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/tailwind.png" className="w-16 h-14" alt="TAILWIND"/>
-                <div className="pt-2 text-center font-bold">TAILWIND</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/react.png" className="w-14 h-14" alt="REACT"/>
-                <div className="pt-2 text-center font-bold">REACT</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/api.png" className="w-14 h-14" alt="API"/>
-                <div className="pt-2 text-center font-bold">API</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/node.png" className="w-14 h-14" alt="NODE"/>
-                <div className="pt-2 text-center font-bold">NODE</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/express.png" className="w-14 h-14" alt="EXPRESS"/>
-                <div className="pt-2 text-center font-bold">EXPRESS</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/mongodb.png" className="w-14 h-14" alt="MONOGODB"/>
-                <div className="pt-2 text-center font-bold">MONOGODB</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/sanity.png" className="w-14 h-14" alt="SANITY"/>
-                <div className="pt-2 text-center font-bold">SANITY</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/python.png" className="w-14 h-14" alt="PYTHON"/>
-                <div className="pt-2 text-center font-bold">PYTHON</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/bash.png" className="w-14 h-14" alt="BASH"/>
-                <div className="pt-2 text-center font-bold">BASH</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/lunix.png" className="w-14 h-14" alt="LUNIX"/>
-                <div className="pt-2 text-center font-bold">LUNIX</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/git.png" className="w-14 h-14" alt="GIT"/>
-                <div className="pt-2 text-center font-bold">GIT</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/github.png" className="w-14 h-14" alt="GITHUB"/>
-                <div className="pt-2 text-center font-bold">GITHUB</div>
-              </motion.div>
-              <motion.div 
-                initial={{opacity:.2,scale:.4}}
-                animate={{opacity:1,scale:1}}
-                transition={{delay:.5}} 
-                whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
-                className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
-              >
-                <img src="tools/c.png" className="w-14 h-14" alt="C"/>
-                <div className="pt-2 text-center font-bold">C</div>
-              </motion.div>
+              {
+                lang_tools?.map((ele,i)=>(
+                  <motion.div 
+                    initial={{opacity:.2,scale:.4}}
+                    animate={{opacity:1,scale:1}}
+                    transition={{delay:.5}} 
+                    key={i}
+                    whileHover={{ scale: 1.2 ,transition:{delay:.05} }}
+                    className="flex flex-col items-center hover:scale-[1.2] hover:cursor-pointer"
+                  >
+                    <img src={ele.img} className="w-14 h-14" alt={ele.name}/>
+                    <div className="pt-2 uppercase text-center font-bold">{ele.name}</div>
+                  </motion.div>
+                ))
+              }
             </div>
           </div>
         </div>
---
+++ src/utils/data.jsx
@@ -0,0 +1,83 @@
+export const edu_exp = [
+    {
+        school:"Baccalaureate",
+        year:"2021 / 2022",
+        desc:"I obtained my Baccalaureate in the field of Natural and life sciences with an average grade of 16.87."
+    },
+    {
+        school:" National higher school of computer science -ESTIN- ",
+        year:"2022 / 2023",
+        desc:"I commenced my studies at the National Higher School of Computer Science Engineering (ESTIN), delving into the foundational aspects of algorithms, data structures, electronics, and mathematics. Throughout my academic journey, I proactively ventured into the realm of web development,mastering both front-end and back-end fundamentals through curated online courses and workshops. "
+    },
+    {
+        school:"National higher school of computer science -ESTIN-",
+        year:"2023 / 2024",
+        desc:"Pending ...."
+    },
+]
+export const lang_tools = [
+    {
+        name: "HTML5",
+        img:"/tools/html.png"
+    },
+    {
+        name: "CSS",
+        img:"/tools/css.png"
+    },
+    {
+        name: "F.MOTION",
+        img:"tools/motion.png"
+    },
+    {
+        name: "JAVASCRIPT",
+        img:"tools/js.png"
+    },
+    {
+        name: "TAILWIND",
+        img:"/tools/tailwind.png"
+    },
+    {
+        name: "REACT",
+        img:"/tools/react.png"
+    },
+    {
+        name: "NODE",
+        img:"/tools/node.png"
+    },
+    {
+        name: "EXPRESS",
+        img:"/tools/express.png"
+    },
+    {
+        name: "MONGODB",
+        img:"/tools/mongodb.png"
+    },
+    {
+        name: "sanity",
+        img:"/tools/sanity.png"
+    },
+    {
+        name: "python",
+        img:"/tools/python.png"
+    },
+    {
+        name: "bash",
+        img:"/tools/bash.png"
+    },
+    {
+        name: "lunix",
+        img:"/tools/lunix.png"
+    },
+    {
+        name: "git",
+        img:"/tools/git.png"
+    },
+    {
+        name: "github",
+        img:"/tools/github.png"
+    },
+    {
+        name: "c",
+        img:"/tools/c.png"
+    },
+]
