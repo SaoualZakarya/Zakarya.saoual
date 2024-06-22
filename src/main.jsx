daa79707e66905c59574eecfafddc22fec0d56e5
zakaryasaoual@gmail.com <zakaryasaoual@gmail.com>
2024-06-22 17:56:42 +0100

    fix the posthog

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import { PostHogProvider} from 'posthog-js/react'
 posthog.init(import.meta.env.VITE_POSTHOG_KEY,
   {
       api_host: import.meta.env.VITE_POSTHOG_HOST,
-      capture_pageview:false,
       persistence: 'localStorage',
   }
 )
